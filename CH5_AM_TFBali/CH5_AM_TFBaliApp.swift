2bdb91ce1bf90f29078dae5447f07f0db1fcc912
noviarn <noviarhmns@gmail.com>
2026-08-10 11:03:50 +0800

    Initial Commit

---
+++ CH5_AM_TFBali/CH5_AM_TFBaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  CH5_AM_TFBaliApp.swift
+//  CH5_AM_TFBali
+//
+//  Created by Novia Rahman Nisa on 10/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CH5_AM_TFBaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CH5_AM_TFBali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CH5_AM_TFBali
+//
+//  Created by Novia Rahman Nisa on 10/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
