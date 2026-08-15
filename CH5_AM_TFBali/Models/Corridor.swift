e837429c335397208875bb7a48f133b5c1958749
Pafras <pafras.prayogo@gmail.com>
2026-08-15 11:04:37 +0800

    feat: add BusStop, RouteDirection, Corridor models

---
+++ CH5_AM_TFBali/Models/Corridor.swift
@@ -0,0 +1,28 @@
+import Foundation
+import CoreLocation
+import SwiftUI
+
+struct BusStop: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
+struct RouteDirection: Identifiable {
+    let id = UUID()
+    let label: String
+    let stops: [BusStop]
+    var viaPoints: [Int: [CLLocationCoordinate2D]] = [:]
+    var manualOverride: [Int: [CLLocationCoordinate2D]] = [:]
+}
+
+struct Corridor: Identifiable {
+    let id: String
+    let name: String
+    let color: Color
+    let directions: [RouteDirection]
+}
+
+func stop(_ name: String, _ lat: Double, _ lon: Double) -> BusStop {
+    BusStop(name: name, coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon))
+}
