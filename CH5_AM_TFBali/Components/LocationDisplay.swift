7d13ca93f49bfd054f78402adb7359442164f5de
noviarn <noviarhmns@gmail.com>
2026-08-19 11:44:22 +0800

    changed the locationdisplay component from navigationlink to just hstack

--- CH5_AM_TFBali/Components/LocationDisplay.swift
+++ CH5_AM_TFBali/Components/LocationDisplay.swift
@@ -11,25 +11,21 @@ struct LocationDisplay: View {
     @State private var locationManager = LocationManager()
     
     var body: some View {
-        NavigationLink {
-            // user's location selection screen tba
-        } label: {
-            HStack(spacing: 5) {
-                Image(systemName: "mappin.and.ellipse")
-                    .font(.system(size: 16))
-                Text(locationManager.cityName)
-                    .font(.system(.body, design: .rounded))
-                    .fontWeight(.semibold)
-            }
-            .padding(.horizontal, 15)
-            .padding(.vertical, 8)
-            .foregroundStyle(.white)
-            .background(Color.primaryPurple)
-            .clipShape(Capsule())
+        HStack(spacing: 5) {
+            Image(systemName: "mappin.and.ellipse")
+                .font(.system(size: 16))
+            Text(locationManager.cityName)
+                .font(.system(.body, design: .rounded))
+                .fontWeight(.semibold)
         }
         .onAppear {
             locationManager.requestLocation()
         }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 8)
+        .foregroundStyle(.white)
+        .background(Color.primaryPurple)
+        .clipShape(Capsule())
     }
 }
 
