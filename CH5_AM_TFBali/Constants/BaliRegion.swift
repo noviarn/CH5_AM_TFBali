0bc56dc3ad9aceb97c485aeea1da37113475a752
Novia <42713448+noviarn@users.noreply.github.com>
2026-08-25 17:41:55 +0800

    Revert "Fix/UI pt 2"

--- CH5_AM_TFBali/Constants/BaliRegion.swift
+++
@@ -1,6 +0,0 @@
-import MapKit
-
-let baliRegion = MKCoordinateRegion(
-    center: CLLocationCoordinate2D(latitude: -8.66, longitude: 115.21),
-    span: MKCoordinateSpan(latitudeDelta: 0.45, longitudeDelta: 0.45)
-)
